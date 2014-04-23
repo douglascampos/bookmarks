b076e59bddec2e82cc287538f49c348234c3586f
Douglas C Campos <camargo@gmail.com>
2014-04-23 18:58:15 -0300

    Generate new schema DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423135956) do
+ActiveRecord::Schema.define(version: 20140423215624) do
 
   create_table "bookmarks", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140423135956) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   create_table "category", force: true do |t|
