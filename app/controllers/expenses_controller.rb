35b78bdc656ad780ddd892c7149207f1b6243c68
Douglas C Campos <camargo@gmail.com>
2014-04-24 21:06:57 -0300

    Change expense to show only user expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,7 +5,7 @@ class ExpensesController < ApplicationController
   # GET /expenses
   # GET /expenses.json
   def index
-    @expenses = Expense.all
+    @expenses = current_user.expenses
   end
 
   # GET /expenses/1
