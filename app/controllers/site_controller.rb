8fec101efe17131e34ac37d4e431400dd88e3fef
Douglas C Campos <camargo@gmail.com>
2014-04-25 21:17:53 -0300

    change home page to show expenses and bookmarks

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,8 +1,12 @@
 class SiteController < ApplicationController
+	before_action :authenticate_user!
   def index
 		# retrieve all Bookmarks ordered by descending creation timestamp
 		
 		# TODO: Validar se vale a pena listar todos os bookmarks de todos os clientes ou apenas os bookmarks do cliente logado
+
 		@bookmarks = current_user.bookmarks.order('created_at desc')
+
+		@expenses = current_user.expenses#.order('created_at desc')
   end
 end
